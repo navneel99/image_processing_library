002acf67768dc1a2c929e7af12074d1a34d3a433
Navneel <navneel99@gmail.com>
2019-01-11 00:22:38 +0530

    Added Softmax Activation

--- Task1.cpp
+++ Task1.cpp
@@ -33,8 +33,20 @@ void MaxPooling(){
 
 }
 
-void Softmax(){
-
+vector<float> Softmax(vector<float> v){
+    vector<float> v2;
+    float pden = 0.0;
+    for(int i = 0; i < v.size();i++){
+        float temp; 
+        temp = exp(v.at(i));
+        pden+=temp;
+        v2.push_back(temp);
+    }
+    for (int i = 0; i < v2.size();i++){
+        v2[i] = v2[i]/pden;
+    }
+    cout << v2[0]<<" "<<v2[1]<<endl;
+    return v2;
 }
 
 void Sigmoid(){
@@ -74,6 +86,9 @@ int main(){
         a.push_back(ele);
     }
     b = Tanh(a);
+    
+    vector<float> a = {1,2,3,4};
+    a = Softmax(a);
     */
 return 0;
 }
