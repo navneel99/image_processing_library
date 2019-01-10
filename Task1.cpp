2b08b873bc0f85ed1aa61915aa97578eb12313f5
high-functioning-sociopath-sherlock <33995409+high-functioning-sociopath-sherlock@users.noreply.github.com>
2019-01-10 15:04:22 +0530

    First Commit

---
+++ Task1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void main(){
+
+}
+
+void Convolution(){
+
+} 
+
+void Relu(){
+
+} 
+
+void Tanh(){
+
+}
+
+void MaxPooling(){
+
+}
+
+void Softmax(){
+
+}
+
+void Sigmoid(){
+    
+}
