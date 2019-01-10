f54f3b54637642c0b0baeecb0f41179f55076163
high-functioning-sociopath-sherlock <33995409+high-functioning-sociopath-sherlock@users.noreply.github.com>
2019-01-11 01:20:58 +0530

    My first pull

--- Task1.cpp
+++ Task1.cpp
@@ -2,11 +2,32 @@
 
 using namespace std;
 
-void main(){
-
+void Convolution(vector<vector<int>>arr, vector<vector<int>>kernel, int x);
+int main(){
+  
+  return 0;
 }
 
-void Convolution(){
+void Convolution(vector<vector<float>>arr, vector<vector<float>>kernel, int x){
+  
+  int ArrSize = arr.size();     //No. of rows
+
+  int KernelSize = kernel.size(); //No. of rows
+  
+  int mid = (KernelSize + 1)/2;  //Finding the mid element of the kernel
+
+  //Range of the mid to move 
+  int x1 = (KernelSize-1)/2;
+  int x2 = ArrSize - (KernelSize+1)/2;
+
+  for(int i=x1; i<x2; i++){
+    for(int j=x1; j<x2; j++){
+      float term = 0;
+      for(int m=i-x1; m<i+x1){
+       
+      }
+    }
+  }
 
 } 
 
