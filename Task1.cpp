df092f5497d9d9666c8876849c65dacf32a5d0bd
high-functioning-sociopath-sherlock <33995409+high-functioning-sociopath-sherlock@users.noreply.github.com>
2019-01-11 19:31:48 +0530

    Convolution completed and checked for both even and odd kernel

--- Task1.cpp
+++ Task1.cpp
@@ -43,30 +43,34 @@ int main(){
     */
 return 0;
 }
-/*
+
 void Convolution(vector<vector<float>>arr, vector<vector<float>>kernel, int x){
-  
-  int ArrSize = arr.size();     //No. of rows
+    int n=arr.size();
+  int m=kernel.size();
 
-  int KernelSize = kernel.size(); //No. of rows
-  
-  int mid = (KernelSize + 1)/2;  //Finding the mid element of the kernel
+  int tsize=n-m+1;
+  float O[tsize][tsize];
 
-  //Range of the mid to move 
-  int x1 = (KernelSize-1)/2;
-  int x2 = ArrSize - (KernelSize+1)/2;
-
-  for(int i=x1; i<x2; i++){
-    for(int j=x1; j<x2; j++){
-      float term = 0;
-      for(int m=i-x1; m<i+x1){
-       
+  for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+         float term =0;
+          for(int ii=0; ii<m; ii++){
+              for(int jj=0; jj<m; jj++){
+                if(i+m<=n && j+m<=n){
+                      term = term + arr[i+ii][j+jj]*kernel[ii][jj];
+                }else{
+                  break;
+                }
+              }
+          }
+              if((i+(m-3)/2)<tsize && (j+(m-3)/2)<tsize){
+              O[i+(m-3)/2][j+(m-3)/2] = term;
+              }
       }
-    }
   }
 
 } 
-*/
+
 
 
 vector<vector<float> > Tanh(vector<vector<float> > v){
