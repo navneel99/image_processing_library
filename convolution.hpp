ad009a03d686df3454379ebb77f9be1f7088505b
Navneel <navneel99@gmail.com>
2019-01-16 23:19:34 +0530

    Added separate header files

---
+++ convolution.hpp
@@ -0,0 +1,6 @@
+#ifndef convolution_h
+#define convolution_h
+#include "common.hpp"
+#include "padding.hpp"
+vector<vector<float> > Convolution(vector<vector<float>>arr, vector<vector<float>>kernel, int x);
+#endif
---
+++ io.hpp
@@ -0,0 +1,6 @@
+#ifndef io_h
+#define io_h
+#include "common.hpp"
+vector<vector<float> > inputFromText(string filename, int rows);
+void dispVector(vector<vector<float> > v);
+#endif
