895403b9402631d67617276bc4c10c9c9e7ccd81
Navneel <navneel99@gmail.com>
2019-01-25 03:20:25 +0530

    Pthreads optimized

--- Pthread.cpp
+++ Pthread.cpp
@@ -1,65 +1,67 @@
 #include "convolution.hpp"
 #include <pthread.h>
 
-struct data
-{
-  int row;
-  int column;
-  vector<vector<float> > Matrix;
-  vector<float> kernel;
-  vector<float> result;
-  int m;
-};
-
-
-
-//bigmatrix is of dimension (((n-m)/stride)+1)*(((n-m)/stride)+1) x (m*m) := temp
-// unrolled kernel of size m*mx1 := ker
-
-void* matMulFun(void* arg){
-  data* curr;
-  curr = (data*) arg;
-  int unr_ker_size = (curr->m)*(curr->m);
-  vector<vector<float> > bigmat = curr->Matrix;
-  vector<float> unr_ker = curr->kernel;
-
-  float sum; 
-  sum = 0;
-
-  for (int j =0; j<unr_ker_size;j++){
-    sum += bigmat[curr->row][j] * unr_ker[j];
-  }
   
-  (curr->result)[curr->row] = sum;
-  pthread_exit(NULL);
-}
+// maximum number of threads 
+#define MAX_THREAD 5
+  
+vector<vector<float> > matA;
+vector<float>  matB;
+vector<float>  matC;
+int t;
+int m;
 
-vector<float>  Pthread(vector<vector<float> > temp, vector<float> ker){
-	
-//Performing matrix multiplication and storing the result in the vector of size n-m+1 * n-m+1
-//bigmatrix is of dimension (((n-m)/stride)+1)*(((n-m)/stride)+1) x (m*m) := temp
-// unrolled kernel of size m*mx1 := ker
-int t = temp.size();
-int m = sqrt(ker.size());
-vector<float> may(t);
-
-data curr1;
-curr1.Matrix = temp;
-curr1.kernel = ker;
-curr1.m = m; 
-curr1.row = 0; 
-curr1.column = 1;
-curr1.result = may;
-
-for (int i =0; i< t; i++){
-// j will be 1; the number of columns of the resultant matrix
-  curr1.row = i;
-  pthread_t tid;
-  pthread_create(&tid,NULL,matMulFun, (void * ) &curr1);
-  pthread_join(tid,NULL);
-}
-
-vector<float> rest = curr1.result;
-
-return rest;
+//int matA[MAX][MAX]; 
+//int matB[MAX][MAX]; 
+//int matC[MAX][MAX]; 
+int step_i = 0; 
+  
+void* multi(void* arg) 
+{ 
+    int core = step_i++; 
+    // Each thread computes 1/4th of matrix multiplication 
+    for (int i = core * t/ MAX_THREAD; i < (core + 1) * t / MAX_THREAD; i++) {
+        //for (int j = 0; j < t; j++)  {
+            float sum = 0;
+            for (int k = 0; k < m; k++){
+                sum += matA[i][k] * matB[k];
+            }
+            matC[i] = sum;
+        //}
+    }            
+} 
+  
+// Driver Code 
+//int main()
+vector<float> Pthread(vector<vector<float> > temp, vector<float> ker)
+{ 
+    // Generating random values in matA and matB 
+    t =temp.size();
+    m = ker.size();
+    matA = temp;
+    matB = ker;
+    matC.reserve(t);
+    
+    // declaring four threads 
+    pthread_t threads[MAX_THREAD]; 
+  
+    // Creating four threads, each evaluating its own part 
+    for (int i = 0; i < MAX_THREAD; i++) { 
+        int* p; 
+        pthread_create(&threads[i], NULL, multi, (void*)(p)); 
+    } 
+  
+    // joining and waiting for all threads to complete 
+    for (int i = 0; i < MAX_THREAD; i++)  
+        pthread_join(threads[i], NULL);     
+  
+    // Displaying the result matrix 
+    /*cout << endl 
+         << "Multiplication of A and B" << endl; 
+    for (int i = 0; i < MAX; i++) { 
+        for (int j = 0; j < MAX; j++)  
+            cout << matC[i][j] << " ";         
+        cout << endl; 
+    } */
+    return matC; 
 }
